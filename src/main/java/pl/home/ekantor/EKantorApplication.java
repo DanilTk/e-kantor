e8512449bd3fc91a09993a8fe457c5cec499b9ef
Dan <d.y.tkachenko@gmail.com>
2024-11-17 19:48:41 +0100

    init

---
+++ src/main/java/pl/home/ekantor/EKantorApplication.java
@@ -0,0 +1,13 @@
+package pl.home.ekantor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKantorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EKantorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/home/ekantor/EKantorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.home.ekantor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EKantorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
